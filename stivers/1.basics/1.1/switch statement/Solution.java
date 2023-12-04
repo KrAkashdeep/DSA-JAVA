2d71bbb12a3082c53dd36692f0a8433c67e9aea2
KrAkashdeep <akashdeep93140@gmail.com>
2023-12-04 08:50:46 +0530

    dsa prac

--- stivers/1.basics/1.1/switch statement/Solution.java
+++ stivers/1.basics/1.1/switch statement/Solution.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 public class Solution {
     public static double areaSwitchCase(int ch, double[] a) {
         // Write your code here
-        int i = 0;
-        Scanner sc = new Scanner(System.in);
-        while (i != ch)
-            a[i] = sc.nextInt();
-        i++;
+        double b = 1;
 
-        if (ch == 1) {
-
+        switch (ch) {
+            case 1:
+                b = Math.PI * a[0] * a[0];
+                break;
+            case 2:
+                b = a[0] * a[1];
+                break;
         }
+        return b;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int ch = sc.nextInt();
         double a[] = new double[ch];
-        System.out.println(areaSwitchCase(ch, a));
+        if (ch == 1)
+            System.out.println(areaSwitchCase(ch, a));
+        if (ch == 2)
+            System.out.println(areaSwitchCase(ch, a));
 
     }
 }
