afaca9aabc76e5817abb3ac02f98e4c61787cd2e
KrAkashdeep <akashdeep93140@gmail.com>
2023-12-05 22:27:04 +0530

    palindrome

---
+++ stivers/1.basics/1.4/palindrome/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static boolean palindromeNumber(int n) {
+        // Write your code here.
+        int a = n;
+        int b = 0;
+        while (n != 0) {
+            int c = n % 10;
+            b = b * 10 + c;
+            n = n / 10;
+        }
+        if (a == b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(palindromeNumber(n));
+    }
+}
