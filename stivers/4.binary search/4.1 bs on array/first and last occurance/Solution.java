2db2bc0c8f2b4821dd4d633ada91968e70dadb1f
KrAkashdeep <akashdeep93140@gmail.com>
2023-12-15 20:48:02 +0530

    first and last occurance

---
+++ stivers/4.binary search/4.1 bs on array/first and last occurance/Solution.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Solution {
+
+    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
+        // Write your code here.
+        int a[] = new int[2];
+        a[0] = arr.indexOf(k);
+        a[1] = arr.lastIndexOf(k);
+        return a;
+    }
+
+};
