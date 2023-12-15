37bb6209195abc1436a72e66cc81e21e61a382c1
KrAkashdeep <akashdeep93140@gmail.com>
2023-12-15 20:52:02 +0530

    count occurance

---
+++ stivers/4.binary search/4.1 bs on array/count occurance/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static int count(int arr[], int n, int x) {
+        // Your code goes here
+        int a = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                a++;
+            }
+        }
+        return a;
+    }
+}
