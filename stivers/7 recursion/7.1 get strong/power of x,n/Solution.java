6d7dd9cac0f91d291cd20fc57672d2f3d3926247
KrAkashdeep <akashdeep93140@gmail.com>
2023-12-30 21:51:40 +0530

    power of x,n

---
+++ stivers/7 recursion/7.1 get strong/power of x,n/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static double myPow(double x, int n) {
+        // Write Your Code Here
+        double b = (double) n;
+        double a = Math.pow(x, b);
+        return a;
+    }
+}
