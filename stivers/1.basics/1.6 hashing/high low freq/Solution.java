579c28e476273fbe04277b28c9ecaf10145a6ca4
KrAkashdeep <akashdeep93140@gmail.com>
2024-01-27 21:11:51 +0530

    high low frequency

--- stivers/1.basics/1.6 hashing/high low freq/Solution.java
+++ stivers/1.basics/1.6 hashing/high low freq/Solution.java
@@ -6,10 +6,10 @@ public class Solution {
 
     // }
     public static void main(String[] arg) {
-        int b[] = { 11, 13, 3, 14, 17, 3, 7, 9, 1, 11, 9, 15, 5, 2, 2, 3 };
-
-        Map<Integer, Integer> hs = new HashMap<>();
-        int a[] = new int[2];
+        int b[] = { 12, 17, 16 };
+        // Arrays.sort(b);
+        Map<Integer, Integer> hs = new LinkedHashMap<>();
+        // int a[] = new int[2];
         for (int i = 0; i < b.length; i++) {
             int k = b[i];
             int fr = 0;
@@ -18,29 +18,30 @@ public class Solution {
             fr++;
             hs.put(k, fr);
         }
+        System.out.println(hs);
         // int max = (Collections.max(hs.values()));
         // int min = (Collections.min(hs.values()));
-        int maxF = 0;
-        int minF = b.length;
-        int max = 0, min = 0;
-        for (Map.Entry<Integer, Integer> e : hs.entrySet()) {
-            int c = e.getValue();
-            int el = e.getKey();
+        // int maxF = 0;
+        // int minF = b.length;
+        // int max = 0, min = 0;
+        // for (Map.Entry<Integer, Integer> e : hs.entrySet()) {
+        // int c = e.getValue();
+        // int el = e.getKey();
 
-            if (c > maxF) {
-                max = el;
-                maxF = c;
-            }
-            if (c < minF) {
-                min = el;
-                minF = c;
-            }
-        }
+        // if (c > maxF) {
+        // max = el;
+        // maxF = c;
+        // }
+        // if (c < minF) {
+        // min = el;
+        // minF = c;
+        // }
+        // }
         // a[1] = max;
         // a[0] = min;
         // // return a;
-        System.out.println(max);
-        System.out.println(min);
+        // System.out.println(max);
+        // System.out.println(min);
 
     }
 }
